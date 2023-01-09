d883b59c5112c0979c5d4d2f695e4c6899c7e206
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2023-01-08 18:39:47 -0800

    Code for swerve teleop control
    Done to test driving onto the charge station with a swerve drivetrain

--- Robot2023/src/main/java/com/team2813/frc2023/RobotContainer.java
+++ Robot2023/src/main/java/com/team2813/frc2023/RobotContainer.java
@@ -7,8 +7,11 @@ package com.team2813.frc2023;
 
 import com.team2813.frc2023.Constants.OperatorConstants;
 import com.team2813.frc2023.commands.Autos;
+import com.team2813.frc2023.commands.DefaultDriveCommand;
 import com.team2813.frc2023.commands.ExampleCommand;
+import com.team2813.frc2023.subsystems.Drive;
 import com.team2813.frc2023.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -25,19 +28,28 @@ public class RobotContainer
 {
     // The robot's subsystems and commands are defined here...
     private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+    private final Drive drive = new Drive();
     
-    // Replace with CommandPS4Controller or CommandJoystick if needed
-    private final CommandXboxController driverController =
-            new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
+    private final XboxController driver = new XboxController(0);
     
     
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer()
     {
+        drive.setDefaultCommand(new DefaultDriveCommand(
+                drive,
+                () -> -modifyAxis(driver.getLeftY()) * Drive.MAX_VELOCITY,
+                () -> -modifyAxis(driver.getLeftX()) * Drive.MAX_VELOCITY,
+                () -> -modifyAxis(driver.getRightX()) * Drive.MAX_ANGULAR_VELOCITY
+        ));
+
         // Configure the trigger bindings
         configureBindings();
     }
-    
+
+    Drive getDrive() {
+        return drive;
+    }
     
     /**
      * Use this method to define your trigger->command mappings. Triggers can be created via the
@@ -50,13 +62,7 @@ public class RobotContainer
      */
     private void configureBindings()
     {
-        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-        new Trigger(exampleSubsystem::exampleCondition)
-                .onTrue(new ExampleCommand(exampleSubsystem));
-        
-        // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-        // cancelling on release.
-        driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand());
+
     }
     
     
@@ -69,5 +75,23 @@ public class RobotContainer
     {
         // An example command will be run in autonomous
         return Autos.exampleAuto(exampleSubsystem);
+    }
+
+    private static double deadband(double value, double deadband) {
+        if (Math.abs(value) > deadband) {
+            if (value > 0) {
+                return (value - deadband) / (1 - deadband);
+            } else {
+                return (value + deadband) / (1 - deadband);
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    private static double modifyAxis(double value) {
+        value = deadband(value, 0.1);
+        value = Math.copySign(value * value, value);
+        return value;
     }
 }
