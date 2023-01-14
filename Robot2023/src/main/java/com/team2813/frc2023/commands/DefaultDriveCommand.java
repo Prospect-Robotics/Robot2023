3065f15885be3cafdd086d9c970fde5b18d3baf6
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2023-01-13 16:30:50 -0800

    Create DefaultDriveCommand.java
    DefaultDriveCommand is the command that the drive subsystem is running when there are no other commands for the drive subsystem to run.

---
+++ Robot2023/src/main/java/com/team2813/frc2023/commands/DefaultDriveCommand.java
@@ -0,0 +1,37 @@
+package com.team2813.frc2023.commands;
+
+import com.team2813.frc2023.subsystems.Drive;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import java.util.function.DoubleSupplier;
+
+public class DefaultDriveCommand extends CommandBase {
+    private final Drive driveSubsystem;
+
+    private final DoubleSupplier translationXSupplier;
+    private final DoubleSupplier translationYSupplier;
+    private final DoubleSupplier rotationSupplier;
+
+    public DefaultDriveCommand(Drive driveSubsystem,
+                               DoubleSupplier translationXSupplier,
+                               DoubleSupplier translationYSupplier,
+                               DoubleSupplier rotationSupplier) {
+        this.driveSubsystem = driveSubsystem;
+        this.translationXSupplier = translationXSupplier;
+        this.translationYSupplier = translationYSupplier;
+        this.rotationSupplier = rotationSupplier;
+
+        addRequirements(driveSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        driveSubsystem.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
+                translationXSupplier.getAsDouble(),
+                translationYSupplier.getAsDouble(),
+                rotationSupplier.getAsDouble(),
+                driveSubsystem.getRotation()
+        ));
+    }
+}
