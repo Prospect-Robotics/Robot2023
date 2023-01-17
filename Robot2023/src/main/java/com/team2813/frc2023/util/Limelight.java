86413c66e23c7dee79c5cfbe7c1e8d9b90300668
cuttestkittensrule <78694502+cuttestkittensrule@users.noreply.github.com>
2023-01-16 20:10:36 -0800

    Vision (#1)
    * Added LimelightValues.java
    
    * Added Limelight.java
    
    * removed function for getting position using relative position to Apriltags
    
    * WIP: get the json from Limelight
    
    * made the LimelightValues code a bit better (harder to use incorrectly)
    
    * added functionality to see your x and y position on the field (field view), and made it harder to use the code in LimelightValues.java incorrectly
    
    * updated gitignore to ignore some unwanted files
    
    * Made sure that getFieldLocation() in LimelightValues always returns an array of length 6, and added documentation for the same method
    
    * Update WPILib
    
    * Added some tests for LimelightValues.java
    
    * Added functionality for seeing values on SmartDashboard
    
    * Changed the coordanate system exposed by Limelight.java to use the same system as PathPlanner
    
    Additionally, Limelight.java is now using Pose2d instead of a Double[]
    
    * Update team number
    
    Temporarily change team number to 9813 since we are testing Limelight on Playground Monke.
    
    * Changed getPosition to use Optionals
    
    * Correcting construction of Posed2d
    
    * Making updates to Limelight telemetry
    
    Co-authored-by: abhineetpal <57788065+abhineetpal@users.noreply.github.com>

---
+++ Robot2023/src/main/java/com/team2813/frc2023/util/Limelight.java
@@ -0,0 +1,69 @@
+package com.team2813.frc2023.util;
+
+import java.util.Optional;
+
+import com.team2813.lib.util.LimelightValues;
+import com.team2813.lib.util.LimelightValues.LedState;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Limelight extends SubsystemBase {
+
+    private final LimelightValues values = new LimelightValues();
+
+    private static final double MOUNT_ANGLE = 0; // degrees
+    private static final double MOUNT_HEIGHT = 0; // inches
+    private static final double TARGET_HEIGHT = 0; // inches
+
+    private Limelight() {
+        //
+    }
+
+    private static final Limelight instance = new Limelight();
+
+    public static Limelight getInstance() {
+        return instance;
+    }
+
+    public LimelightValues getValues() {
+        return values;
+    }
+
+    public void setLights(boolean enable) {
+        values.setLedState(enable ? LedState.DEFAULT : LedState.OFF);
+    }
+
+    public void setStream(int stream) {
+        values.getStream().setNumber(stream);
+    }
+
+    /**
+     * Gets the position on the playing field using build-in robot localization
+     */
+    // Do the output how you want to. (but make it make sense)
+    public Optional<Pose2d> getPosition() {
+        if (values.hasTargets()) {
+            Double[] location = values.getFieldLocation();
+            location[0] = location[0] + 8.27;
+            location[1] = location[1] + 4.01;
+            Pose2d pose = new Pose2d(location[0], location[1], Rotation2d.fromDegrees(location[5]));
+            return Optional.of(pose);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void periodic() {
+        getPosition().ifPresent((Pose2d position) -> {
+            SmartDashboard.putNumber("x position (meters):", position.getX());
+            SmartDashboard.putNumber("y position (meters):", position.getY());
+            SmartDashboard.putNumber("robot heading (degrees):", position.getRotation().getDegrees());
+        });
+        SmartDashboard.putBoolean("Valid apriltag", values.hasTargets());
+        SmartDashboard.putNumber("Id of primary AprilTag", values.primaryApriltag());
+    }
+}
