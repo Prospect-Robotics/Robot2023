28241057afd035b7be88ee45d7d3d6d6e43014f0
Vineet Rao <60551332+vineetrao06@users.noreply.github.com>
2023-01-26 16:43:13 -0800

    Create Arm.java

---
+++ Robot2023/src/main/java/com/team2813/frc2023/subsystems/Arm.java
@@ -0,0 +1,20 @@
+package com.team2813.frc2023.subsystems;
+
+import com.team2813.lib.motors.SparkMaxWrapper;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.team2813.frc2023.subsystems.Subsystem1d;
+
+public class Arm extends Subsystem1d {
+    public Arm(SparkMaxWrapper motor) {
+        super(motor);
+    }
+
+    // Initialize motors
+
+    // Methods
+
+    void extend ()
+
+
+}
