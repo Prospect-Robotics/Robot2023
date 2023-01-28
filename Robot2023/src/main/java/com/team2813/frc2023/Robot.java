ce749ff9c6ffaa44736fcc0dc639d6bf5f0b7172
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2023-01-28 00:43:52 -0800

    Limelight lights should turn off on disabled

--- Robot2023/src/main/java/com/team2813/frc2023/Robot.java
+++ Robot2023/src/main/java/com/team2813/frc2023/Robot.java
@@ -65,7 +65,9 @@ public class Robot extends TimedRobot
     
     /** This method is called once each time the robot enters Disabled mode. */
     @Override
-    public void disabledInit() {}
+    public void disabledInit() {
+        limelight.setLights(false);
+    }
     
     
     @Override
