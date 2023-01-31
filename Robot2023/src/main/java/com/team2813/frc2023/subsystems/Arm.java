21bf7890bcd595eac4d7c6f9ce2037c59e3ed0f2
Amrik Verma <amrikverma23@gmail.com>
2023-01-30 18:55:54 -0800

    Arm code started
    Co-Authored-By: KaptainKermit <65127786+MarkoKupresanin@users.noreply.github.com>

--- Robot2023/src/main/java/com/team2813/frc2023/subsystems/Arm.java
+++ Robot2023/src/main/java/com/team2813/frc2023/subsystems/Arm.java
@@ -1,20 +1,60 @@
 package com.team2813.frc2023.subsystems;
 
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.team2813.lib.motors.SparkMaxWrapper;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.team2813.frc2023.subsystems.Subsystem1d;
+import com.team2813.lib.solenoid.SolenoidGroup;
 
 public class Arm extends Subsystem1d {
-    public Arm(SparkMaxWrapper motor) {
-        super(motor);
+
+    private final SolenoidGroup pistons = new SolenoidGroup(PCM_ID, PneumaticsModuleType.CTREPCM, 0, 1);
+
+    public Arm() {
+        super(new SparkMaxWrapper(CLIMBER_ID, SparkMaxInvertType.Clockwise));
+
+        motor.configPID(0.4, 0, 0); //probably will need to change
+        motor.configMotionMagic(30000, 30000); // max vel in ticks/100ms
+
+        setPosition(Position.RETRACTED);
+    }
+   
+    public void periodic() {
+        //add as needed
     }
 
-    // Initialize motors
+    public double getMotorVelocity(){
+        return motor.getVelocity();
+    }
 
-    // Methods
+    public boolean positionReached() {
+        return Math.abs(currentPosition.getPos() - motor.getEncoderPosition()) < 0.05; //probably change value
+    }
 
-    void extend ()
+    public void extendPistons() {
+        pistons.set(SolenoidGroup.PistonState.EXTENDED);
+    }
+
+    public void retractPistons() {
+        pistons.set(SolenoidGroup.PistonState.RETRACTED);
+    }
+
+    public void startLoweringClimber() {
+        motor.set(ControlMode.DUTY_CYCLE, -0.98); //probably change value
+    }
+
+    public void brake() {
+        motor.set(ControlMode.DUTY_CYCLE, 0);
+    }
+
+    public enum extensionLength extends Subsystem1d.Position {
+        INTAKE(26),
+        MIDDLE(62.25),
+        TOP(136.6),
+        DOUBLE_SUBSTATION(80);
+    }
+    // void extend ()
 
 
 }
