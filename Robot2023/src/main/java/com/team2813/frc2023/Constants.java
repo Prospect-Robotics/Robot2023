e51b002317f1620f21fa818995628f120027382b
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2023-01-31 16:39:44 -0800

    Fixed errors

--- Robot2023/src/main/java/com/team2813/frc2023/Constants.java
+++ Robot2023/src/main/java/com/team2813/frc2023/Constants.java
@@ -19,4 +19,6 @@ public final class Constants
     {
         public static final int DRIVER_CONTROLLER_PORT = 0;
     }
+
+    public static int ARM_ID = 16;
 }
--- Robot2023/src/main/java/com/team2813/frc2023/subsystems/Arm.java
+++ Robot2023/src/main/java/com/team2813/frc2023/subsystems/Arm.java
@@ -1,27 +1,18 @@
 package com.team2813.frc2023.subsystems;
 
-import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.team2813.lib.motors.ControlMode;
 import com.team2813.lib.motors.SparkMaxWrapper;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.team2813.frc2023.subsystems.Subsystem1d;
-import com.team2813.lib.solenoid.SolenoidGroup;
+
+import static com.team2813.frc2023.Constants.*;
 
 public class Arm extends Subsystem1d {
 
-    private final SolenoidGroup pistons = new SolenoidGroup(PCM_ID, PneumaticsModuleType.CTREPCM, 0, 1);
-
     public Arm() {
-        super(new SparkMaxWrapper(CLIMBER_ID, SparkMaxInvertType.Clockwise));
+        super(new SparkMaxWrapper(ARM_ID, MotorType.kBrushless, true));
 
         motor.configPID(0.4, 0, 0); //probably will need to change
         motor.configMotionMagic(30000, 30000); // max vel in ticks/100ms
-
-        setPosition(Position.RETRACTED);
-    }
-   
-    public void periodic() {
-        //add as needed
     }
 
     public double getMotorVelocity(){
@@ -32,14 +23,6 @@ public class Arm extends Subsystem1d {
         return Math.abs(currentPosition.getPos() - motor.getEncoderPosition()) < 0.05; //probably change value
     }
 
-    public void extendPistons() {
-        pistons.set(SolenoidGroup.PistonState.EXTENDED);
-    }
-
-    public void retractPistons() {
-        pistons.set(SolenoidGroup.PistonState.RETRACTED);
-    }
-
     public void startLoweringClimber() {
         motor.set(ControlMode.DUTY_CYCLE, -0.98); //probably change value
     }
@@ -48,13 +31,31 @@ public class Arm extends Subsystem1d {
         motor.set(ControlMode.DUTY_CYCLE, 0);
     }
 
-    public enum extensionLength extends Subsystem1d.Position {
+    public enum ExtensionLength implements Position {
         INTAKE(26),
         MIDDLE(62.25),
         TOP(136.6),
         DOUBLE_SUBSTATION(80);
+
+        @Override
+        public double getPos() {
+            return position;
+        }
+
+        @Override
+        public Position getMin() {
+            return INTAKE;
+        }
+
+        @Override
+        public Position getMax() {
+            return TOP;
+        }
+
+        final double position;
+
+        ExtensionLength(double position ) {
+            this.position = position;
+        }
     }
-    // void extend ()
-
-
 }
