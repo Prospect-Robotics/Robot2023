193761e52a093f7369e266968278de80f09eb06f
abhineetpal <57788065+abhineetpal@users.noreply.github.com>
2023-02-01 17:08:30 -0800

    Added piston to intake code
    Forgot about the piston

--- Robot2023/src/main/java/com/team2813/frc2023/Constants.java
+++ Robot2023/src/main/java/com/team2813/frc2023/Constants.java
@@ -22,4 +22,7 @@ public final class Constants
 
     public static final int INTAKE_MASTER_ID = 15;
     public static final int INTAKE_FOLLOWER_ID = 16;
+    public static final int PCM_ID = 17;
+
+    public static final int INTAKE_PISTON_CHANNEL = 0;
 }
--- Robot2023/src/main/java/com/team2813/frc2023/subsystems/Intake.java
+++ Robot2023/src/main/java/com/team2813/frc2023/subsystems/Intake.java
@@ -3,16 +3,27 @@ package com.team2813.frc2023.subsystems;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.team2813.lib.motors.ControlMode;
 import com.team2813.lib.motors.SparkMaxWrapper;
+import com.team2813.lib.solenoid.SolenoidGroup;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static com.team2813.frc2023.Constants.*;
 
 public class Intake extends SubsystemBase {
     // Neo 550 and Falcon motors are brushless, kBrushless enum value - refer to MotorType class 
-    public final SparkMaxWrapper motor = new SparkMaxWrapper(INTAKE_MASTER_ID, true, MotorType.kBrushless);
+    private final SparkMaxWrapper motor = new SparkMaxWrapper(INTAKE_MASTER_ID, true, MotorType.kBrushless);
+    private final SolenoidGroup piston = new SolenoidGroup(PCM_ID, PneumaticsModuleType.CTREPCM, INTAKE_PISTON_CHANNEL);
 
     public Intake () {
         motor.addFollower(INTAKE_FOLLOWER_ID, MotorType.kBrushless, true);
+    }
+
+    public void open() {
+        piston.retract();
+    }
+
+    public void close() {
+        piston.extend();
     }
 
     public void intake () {
