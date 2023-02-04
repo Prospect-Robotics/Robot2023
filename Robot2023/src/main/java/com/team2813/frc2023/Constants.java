ef46b0f7dcd1a1179485aab2cfbd8fa4a1e5cb11
KaptainKermit <kupresaninmarko2@gmail.com>
2023-02-03 17:00:30 -0800

    Update Constants.java

--- Robot2023/src/main/java/com/team2813/frc2023/Constants.java
+++ Robot2023/src/main/java/com/team2813/frc2023/Constants.java
@@ -13,10 +13,11 @@ package com.team2813.frc2023;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants
-{
-    public static class OperatorConstants
-    {
+public final class Constants {
+    public static class OperatorConstants {
         public static final int DRIVER_CONTROLLER_PORT = 0;
     }
+
+    public static int MOTOR_WRIST_ID = 17;
+
 }
