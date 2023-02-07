f87586e463986a59b0846286940898177bc4efe9
Vineet Rao <60551332+vineetrao06@users.noreply.github.com>
2023-02-06 16:47:41 -0800

    added piston retract/extend functions

--- Robot2023/src/main/java/com/team2813/frc2023/subsystems/Spatula.java
+++ Robot2023/src/main/java/com/team2813/frc2023/subsystems/Spatula.java
@@ -1,12 +1,23 @@
 package com.team2813.frc2023.subsystems;
 
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.team2813.lib.motors.SparkMaxWrapper;
+import com.team2813.lib.solenoid.SolenoidGroup;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static com.team2813.frc2023.Constants.*;
+
 public class Spatula extends SubsystemBase {
-    // Define the motors
 
-    public Spatula() {}
-    public void extend() {}
+    private final SparkMaxWrapper motor = new SparkMaxWrapper(INTAKE_MASTER_ID, CANSparkMaxLowLevel.MotorType.kBrushless, true);
+    private final SolenoidGroup piston = new SolenoidGroup(PCM_ID, PneumaticsModuleType.CTREPCM, INTAKE_PISTON_CHANNEL);
+    public void open() {
+        piston.extend(); // actually retracts the piston
+    }
 
-    public void retract() {}
+    public void close() {
+        piston.retract(); // actually extends the piston
+    }
+
 }
