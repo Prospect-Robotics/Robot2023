497623e3c6edad24734b944311927fe9014fe958
Vineet Rao <60551332+vineetrao06@users.noreply.github.com>
2023-02-06 16:50:08 -0800

    finished spatula code

--- Robot2023/src/main/java/com/team2813/frc2023/subsystems/Spatula.java
+++ Robot2023/src/main/java/com/team2813/frc2023/subsystems/Spatula.java
@@ -14,11 +14,11 @@ public class Spatula extends SubsystemBase {
 
     private final SparkMaxWrapper motor = new SparkMaxWrapper(INTAKE_MASTER_ID, CANSparkMaxLowLevel.MotorType.kBrushless, true);
     private final SolenoidGroup piston = new SolenoidGroup(PCM_ID, PneumaticsModuleType.CTREPCM, INTAKE_PISTON_CHANNEL);
-    public void open() {
+    public void extend() {
         piston.extend(); // actually retracts the piston
     }
 
-    public void close() {
+    public void retract() {
         piston.retract(); // actually extends the piston
     }
 
