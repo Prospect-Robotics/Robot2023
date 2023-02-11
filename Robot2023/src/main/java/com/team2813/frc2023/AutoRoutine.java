cf374624aa23045c8c96d4600ae7e1c27347ecd6
Vineet Rao <60551332+vineetrao06@users.noreply.github.com>
2023-02-10 18:35:26 -0800

    Update AutoRoutine.java

--- Robot2023/src/main/java/com/team2813/frc2023/AutoRoutine.java
+++ Robot2023/src/main/java/com/team2813/frc2023/AutoRoutine.java
@@ -13,11 +13,19 @@ public enum AutoRoutine {
 //    L2_1CO_INTAKE1("L2-1Co-Intake1", AUTO_FACTORY.buildPathGroupAuto("L2 - 1Co - Intake1")),
 //    L3_1CO_1CU("L3-1Co-1Cu", AUTO_FACTORY.buildPathGroupAuto("L3 - 1Co - 1Cu")),
 //    L3_1CO_CLIMB("L3-1Co-Climb", AUTO_FACTORY.buildTrajectoryAuto("L3 - 1Co - Climb")),
+    // Kyle
     L1_1C_GET_AWAY("L1-1C-Get out of the way", AUTO_FACTORY.buildTrajectoryAuto("L1 - 1C - Get out of the way")),
+
+    // Michelle
     L3_MOBILITY("L3-Mobility", AUTO_FACTORY.buildTrajectoryAuto("L3 - Mobility")),
+
+    // Wendy
     L1_MOBILITY_GET_AWAY("L1-Mobility-Get out of the way", AUTO_FACTORY.buildTrajectoryAuto("L3 - Mobility - Get out of the way")),
+
+    // Vineet
+    L1_1_CO_CLIMB("L1-1Co-Climb", AUTO_FACTORY.buildTrajectoryAuto("L1 - 1Co - Climb")),
+
     ROTATE_TEST("Rotate Test", new RotateCommand(90, ROBOT_CONTAINER.getDrive()));
-    
     private final String name;
     private final Command command;
 
