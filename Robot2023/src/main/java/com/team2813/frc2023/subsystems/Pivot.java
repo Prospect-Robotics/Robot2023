3be895f0c45f3085a2c7a61c4b2d2d75d540a5de
Vineet Rao <60551332+vineetrao06@users.noreply.github.com>
2023-02-16 16:15:37 -0800

    Update Pivot.java

--- Robot2023/src/main/java/com/team2813/frc2023/subsystems/Pivot.java
+++ Robot2023/src/main/java/com/team2813/frc2023/subsystems/Pivot.java
@@ -2,6 +2,26 @@ package com.team2813.frc2023.subsystems;
 
 
 
-public class Pivot extends Subsystem1d<> {
+public class Pivot extends Subsystem1d<Pivot.Rotations> {
+
+    public enum Rotations implements Position {
+        INTAKE(5.99),
+        CUBE_OUTAKE(13.32),
+        CONE_OUTAKE(13.32 ),
+        RESET(0);
+        // TODO: EXACT VALUES TBD (calculations might be off)
+
+        @Override
+        public double getPos() {
+            return position;
+        }
+
+        final double position;
+
+        Rotations(double position ) {
+            this.position = position;
+        }
+    }
+
 
 }
